c6762cd133ff55b5c796c9d3c3221fe5fe09c192
brfish <1748065414@qq.com>
2021-04-17 20:31:36 +0800

    Init

---
+++ README.md
@@ -0,0 +1,13 @@
+no-flash-plz !
+======
+
+## 0. Introduction
+
+使用 HTML5 播放器 [videojs](https://github.com/videojs/video.js) 替换学在西电课程回放原本的 Flash 播放器。装 Flash 是不可能装的，这辈子不可能装的
+
+## 1. Quick start
+
+你需要一个油猴插件 [Tampermonkey](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo) 来运行这个脚本，你可以在 Chrome 网上应用店或者其他什么地方找到它
+
+你也可以直接通过 [GreasyFork](https://greasyfork.org/zh-CN/scripts/425163-no-flash-plz) 快速地安装它
+
---
+++ no-flash-plz.js
@@ -0,0 +1,69 @@
+// ==UserScript==
+// @name         no-flash-plz
+// @namespace    https://github.com/brfish
+// @version      0.1
+// @description  使用 HTML5 播放器替换学在西电课程回放平台原本的 Flash 播放器
+// @author       brfish
+// @match        http://newes.learning.xidian.edu.cn/threepart/index.html*
+
+// ==/UserScript==
+
+function requireJS(src) {
+    let script = document.createElement("script");
+    script.src = src;
+    document.head.appendChild(script);
+    return script;
+}
+
+function requireCSS(rel, href) {
+    let css = document.createElement("link");
+    css.rel = rel;
+    css.href = href;
+    document.head.appendChild(css);
+    return css;
+}
+
+function getVideoSource() {
+    let result = window.location.search.substr(1).match(
+        new RegExp("(^|&)info=([^&]*)(&|$)")
+    );
+    let infoStr = decodeURIComponent(result[2]);
+    let info = JSON.parse(infoStr);
+    return info.videoPath.mobile;
+}
+
+function main() {
+    let jsExtern = {
+        videojs: requireJS("https://unpkg.com/video.js/dist/video.js"),
+        videojsHS: requireJS("https://unpkg.com/@videojs/http-streaming/dist/videojs-http-streaming.js"),
+    };
+    let videojsCSS = requireCSS("stylesheet", "https://unpkg.com/video.js/dist/video-js.css");
+
+    jsExtern.videojs.onload = function() {
+        let player = videojs("h5VideoPlayer", {
+            bigPlayButton: true,
+            playbackRates: [0.5, 1, 1.5, 2, 2.5, 3],
+            loop: false,
+            preload: "auto",
+        });
+    }
+
+    let videoPlayer = document.createElement("video");
+    videoPlayer.id = "h5VideoPlayer";
+    videoPlayer.width = document.body.clientWidth;
+    videoPlayer.height = document.body.clientHeight;
+    videoPlayer.className = "video-js vjs-default-skin";
+    videoPlayer.controls = true;
+
+    let videoSource = document.createElement("source");
+    videoSource.src = getVideoSource();
+    videoSource.type = "application/x-mpegURL";
+    videoPlayer.appendChild(videoSource);
+
+    let oldVideoPlayer = document.getElementsByClassName("install-flash");
+    oldVideoPlayer[0].replaceChild(videoPlayer, oldVideoPlayer[0].children[0]);
+}
+
+(function() {
+    main();
+})();
