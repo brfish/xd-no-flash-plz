bb584319174cb80227e2cd41e9c958f8452fe6e5
inkhog <57040318+inkhog@users.noreply.github.com>
2021-05-08 22:08:40 +0800

    Update xd-no-flash-plz.js
    部分回放会用.../index1.html的url

--- xd-no-flash-plz.js
+++ xd-no-flash-plz.js
@@ -4,8 +4,8 @@
 // @version      0.4
 // @description  使用 HTML5 播放器替换学在西电课程回放平台原本的 Flash 播放器
 // @author       brfish
-// @match        http://newes.learning.xidian.edu.cn/threepart/index.html*
-// @match        http://newes.chaoxing.com/threepart/index.html*
+// @match        http://newes.learning.xidian.edu.cn/threepart/index*
+// @match        http://newes.chaoxing.com/threepart/index*
 
 // ==/UserScript==
 
